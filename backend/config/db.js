ee192800627b856260108e32988230ddad7313f1
kevinlelrula-max <kevinlelrula@gmail.com>
2026-05-03 01:47:32 -0500

    fix: variables de entorno para produccion

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port:     process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 export default pool;
