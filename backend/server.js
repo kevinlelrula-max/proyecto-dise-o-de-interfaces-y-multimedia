09e00a44283e5fc473aa2f4b62bab1223aac9daf
kevinlelrula-max <kevinlelrula@gmail.com>
2026-05-11 03:31:10 -0500

    correcciones logica

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}`);
-  
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor corriendo en puerto: ${PORT}`);
 });
 
